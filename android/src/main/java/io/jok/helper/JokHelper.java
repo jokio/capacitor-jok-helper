0b170d66d6eb3070ab20c7d7eb6d26e6dd792b6c
Ezeki Zibzibadze <ez@jok.io>
2020-09-25 19:04:07 +0400

    feat: wip server implementation

--- android/src/main/java/io/jok/helper/JokHelper.java
+++ android/src/main/java/io/jok/helper/JokHelper.java
@@ -20,8 +20,39 @@ public class JokHelper extends Plugin {
 }
 
 
+// package io.jok.helper
 
-//  @PluginMethod
+// import android.app.Activity
+// import android.content.ActivityNotFoundException
+// import android.content.Context
+// import android.content.Intent
+// import android.net.Uri
+// import android.util.DisplayMetrics
+// import androidx.core.content.ContextCompat.startActivity
+// import com.android.billingclient.api.*
+// import com.android.billingclient.api.BillingClient.SkuType.INAPP
+// import com.android.billingclient.api.BillingClient.SkuType.SUBS
+// import com.getcapacitor.*
+
+
+// object SingletonClass {
+//   var activity: Activity? = null
+//   var billingClient: BillingClient? = null
+//   var isStoreReady: Boolean = false
+//   var versionName: String = ""
+//   var openAppUrl: (url: Uri) -> Any = { x -> x }
+
+//   val transactionsObservers = mutableListOf<(Purchase) -> Unit>()
+
+//   var publishNewTransaction: (x: Purchase) -> Any = { x ->
+//     transactionsObservers.forEach { it(x) }
+//   }
+// }
+
+
+// @NativePlugin
+// class JokHelper : Plugin() {
+//   @PluginMethod
 //   fun echo(call: PluginCall) {
 //     val value = call.getString("value")
 //     val ret = JSObject()
@@ -31,7 +62,18 @@ public class JokHelper extends Plugin {
 
 //   @PluginMethod
 //   fun setKeychainItem(call: PluginCall) {
+//     val key = call.getString("key")
 //     val value = call.getString("value")
+//     val accessGroup = call.getString("accessGroup")
+
+//     var pref = SingletonClass.activity?.getPreferences(Context.MODE_PRIVATE)
+//     if (pref != null) {
+//       with(pref.edit()) {
+//         putString(key, value)
+//         commit()
+//       }
+//     }
+
 //     val ret = JSObject()
 //     ret.put("value", value)
 //     call.success(ret)
@@ -39,129 +81,337 @@ public class JokHelper extends Plugin {
 
 //   @PluginMethod
 //   fun getKeychainItem(call: PluginCall) {
-//     val value = call.getString("value")
-//     val ret = JSObject()
-//     ret.put("value", value)
-//     call.success(ret)
-//   }
+//     val key = call.getString("key")
 
-//   @PluginMethod
-//   fun listenPushNotificationEvents(call: PluginCall) {
-//     val value = call.getString("value")
-//     val ret = JSObject()
-//     ret.put("value", value)
-//     call.success(ret)
-//   }
+//     var pref = SingletonClass.activity?.getPreferences(Context.MODE_PRIVATE)
+//     var result = pref?.getString(key, null)
 
-//   @PluginMethod
-//   fun isWideScreen(call: PluginCall) {
-//     val value = call.getString("value")
 //     val ret = JSObject()
-//     ret.put("value", value)
+//     ret.put("value", result)
 //     call.success(ret)
 //   }
 
 //   @PluginMethod
 //   fun isMobileDevice(call: PluginCall) {
-//     val value = call.getString("value")
+//     var result = true
+
 //     val ret = JSObject()
-//     ret.put("value", value)
+//     ret.put("value", result)
+//     call.success(ret)
+//   }
+
+//   @PluginMethod
+//   fun isWideScreen(call: PluginCall) {
+//     var metrics = DisplayMetrics()
+
+//     SingletonClass.activity?.windowManager?.defaultDisplay?.getMetrics(metrics)
+
+//     var result = metrics.heightPixels >= 812
+
+//     val ret = JSObject()
+//     ret.put("value", result)
+//     call.success(ret)
+//   }
+
+//   @PluginMethod
+//   fun setDeviceOrientationLock(call: PluginCall) {
+
+//     // NOT IMPLEMENTED. DEPRECATED
+
+//     val ret = JSObject()
+//     ret.put("value", true)
+//     call.success(ret)
+//   }
+
+//   @PluginMethod
+//   fun getDeviceOrientation(call: PluginCall) {
+
+//     // NOT IMPLEMENTED. DEPRECATED
+
+//     val ret = JSObject()
+//     ret.put("isPortrait", true)
+//     ret.put("isFlat", true)
+//     ret.put("isLandscape", false)
+//     ret.put("rawValue", 1)
+//     ret.put("orientation", "portrait")
+//     call.success(ret)
+//   }
+
+//   @PluginMethod
+//   fun listenDeviceOrientationChanges(call: PluginCall) {
+
+//     // NOT IMPLEMENTED. DEPRECATED
+
+//     val ret = JSObject()
+//     ret.put("value", true)
+//     call.success(ret)
+//   }
+
+//   @PluginMethod
+//   fun listenPushNotificationEvents(call: PluginCall) {
+
+//     // NOT IMPLEMENTED
+
+//     val ret = JSObject()
+//     ret.put("value", false)
 //     call.success(ret)
 //   }
 
 //   @PluginMethod
 //   fun getPushNotificationsState(call: PluginCall) {
-//     val value = call.getString("value")
+
+//     // NOT IMPLEMENTED
+
 //     val ret = JSObject()
-//     ret.put("value", value)
+// //    ret.put("value", true)
 //     call.success(ret)
 //   }
 
 //   @PluginMethod
 //   fun askPushNotificationsPermission(call: PluginCall) {
-//     val value = call.getString("value")
+
+//     // NOT IMPLEMENTED
+
 //     val ret = JSObject()
-//     ret.put("value", value)
+// //    ret.put("value", true)
+//     call.success(ret)
+//   }
+
+//   @PluginMethod
+//   fun openAppSettings(call: PluginCall) {
+
+//     // NOT IMPLEMENTED
+
+//     val ret = JSObject()
+//     ret.put("success", false)
 //     call.success(ret)
 //   }
 
 //   @PluginMethod
 //   fun canMakePayments(call: PluginCall) {
-//     val value = call.getString("value")
+
+//     var result = SingletonClass.isStoreReady
+
 //     val ret = JSObject()
-//     ret.put("value", value)
+//     ret.put("success", result)
 //     call.success(ret)
 //   }
+
+
+//   var productsCache: List<SkuDetails>? = null
 
 //   @PluginMethod
 //   fun loadProducts(call: PluginCall) {
-//     val value = call.getString("value")
-//     val ret = JSObject()
-//     ret.put("value", value)
-//     call.success(ret)
+//     val productIds = call.getArray("productIds")
+//     val type = call.getString("type")
+
+//     var skyType: String = if (type == "SUBSCRIPTION") {
+//       INAPP
+//     } else {
+//       SUBS
+//     }
+
+//     val params = SkuDetailsParams.newBuilder()
+//     params.setSkusList(productIds.toList()).setType(skyType)
+
+//     if (SingletonClass.billingClient != null) {
+//       SingletonClass.billingClient?.querySkuDetailsAsync(params.build()) { billingResult, skuDetailsList ->
+//         when (billingResult.responseCode) {
+//           BillingClient.BillingResponseCode.OK -> {
+
+//             this.productsCache = skuDetailsList
+
+//             var items = skuDetailsList?.map { x ->
+//               var res = JSObject()
+//               res.put("title", x.title)
+//               res.put("description", x.description)
+//               res.put("price", x.originalPrice)
+//               res.put("formattedPrice", x.price)
+//               res.put("currencySymbol", x.priceCurrencyCode)
+//               res.put("currencyCode", x.priceCurrencyCode)
+//               res.put("productIdentifier", x.sku)
+//               res.put("isDownloadable", true)
+//               res.put("downloadContentLengths", 0)
+//               res.put("contentVersion", "")
+//               res.put("downloadContentVersion", "")
+
+//               res
+//             }
+
+
+//             val ret = JSObject()
+//             ret.put("success", true)
+//             ret.put("products", JSArray(items))
+//             ret.put("invalidProducts", JSArray())
+//             call.success(ret)
+//           }
+//           else -> {
+//             val ret = JSObject()
+//             ret.put("success", false)
+//             ret.put("products", JSArray())
+//             ret.put("invalidProducts", JSArray())
+//             call.success(ret)
+//           }
+//         }
+//       }
+//     }
+
 //   }
+
 
 //   @PluginMethod
 //   fun requestPayment(call: PluginCall) {
-//     val value = call.getString("value")
+//     val productId = call.getString("productId")
+
+//     if (this.productsCache == null) {
+//       call.reject("PRODUCTS_NOT_LOADED")
+//       return
+//     }
+
+//     var product = this.productsCache?.find { it.sku === productId }
+//     if (product == null) {
+//       call.reject("PRODUCT_NOT_FOUND")
+//       return
+//     }
+
+//     val flowParams = BillingFlowParams.newBuilder()
+//       .setSkuDetails(product)
+//       .build()
+
+//     val responseCode = SingletonClass.billingClient?.launchBillingFlow(activity, flowParams)?.responseCode
+
 //     val ret = JSObject()
-//     ret.put("value", value)
+//     ret.put("success", true)
+//     ret.put("platform", "ANDROID")
+//     ret.put("responseCode", responseCode)
 //     call.success(ret)
 //   }
 
 //   @PluginMethod
 //   fun finishPayment(call: PluginCall) {
-//     val value = call.getString("value")
+//     val transactionId = call.getString("transactionId")
+
+//     var consumablePurchases = SingletonClass.billingClient?.queryPurchases(INAPP)
+//     var purchase = consumablePurchases?.purchasesList?.find { it.orderId === transactionId }
+//     if (purchase !== null) {
+//       val consumeParams =
+//         ConsumeParams.newBuilder()
+//           .setPurchaseToken(purchase.purchaseToken)
+//           .build()
+
+//       SingletonClass.billingClient?.consumeAsync(consumeParams) { billingResult, _ ->
+//         if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+//           val ret = JSObject()
+//           ret.put("success", true)
+//           call.success(ret)
+//         } else {
+//           val ret = JSObject()
+//           ret.put("success", false)
+//           ret.put("message", billingResult.debugMessage)
+//           call.success(ret)
+//         }
+//       }
+
+//       return
+//     }
+
+//     var subscriptionPurchases = SingletonClass.billingClient?.queryPurchases(SUBS)
+//     purchase = subscriptionPurchases?.purchasesList?.find { it.orderId === transactionId }
+
+//     if (purchase !== null) {
+
+//       if (purchase.isAcknowledged) {
+//         val ret = JSObject()
+//         ret.put("success", true)
+//         call.success(ret)
+//       }
+
+//       val acknowledgePurchaseParams = AcknowledgePurchaseParams.newBuilder()
+//         .setPurchaseToken(purchase.purchaseToken)
+
+//       SingletonClass.billingClient?.acknowledgePurchase(acknowledgePurchaseParams.build()) { billingResult ->
+
+//         if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+//           val ret = JSObject()
+//           ret.put("success", true)
+//           call.success(ret)
+//         } else {
+//           val ret = JSObject()
+//           ret.put("success", false)
+//           ret.put("message", billingResult.debugMessage)
+//           call.success(ret)
+//         }
+//       }
+
+//       return
+//     }
+
+
 //     val ret = JSObject()
-//     ret.put("value", value)
+//     ret.put("success", false)
+//     ret.put("message", "Order not found")
 //     call.success(ret)
 //   }
 
 //   @PluginMethod
 //   fun listenTransactionStateChanges(call: PluginCall) {
-//     val value = call.getString("value")
-//     val ret = JSObject()
-//     ret.put("value", value)
-//     call.success(ret)
+
+//     SingletonClass.transactionsObservers.add() { x ->
+//       val ret = JSObject()
+//       ret.put("transactionId", x.orderId)
+//       ret.put("transactionState", x.purchaseState)
+//       ret.put("transactionReceipt", x.purchaseToken)
+//       ret.put("productId", x.sku)
+//       ret.put("platform", "ANDROID")
+//       ret.put("hasError", false)
+//       ret.put("errorCode", "")
+//       ret.put("errorMessage", "")
+//       ret.put("platform", "ANDROID")
+
+//       this.notifyListeners("TransactionStateChange", ret)
+//     }
+
+//     var consumablePurchases = SingletonClass.billingClient?.queryPurchases(INAPP)
+//     var subscriptionPurchases = SingletonClass.billingClient?.queryPurchases(SUBS)
+
+//     consumablePurchases?.purchasesList?.forEach { SingletonClass.publishNewTransaction(it) }
+//     subscriptionPurchases?.purchasesList?.forEach { SingletonClass.publishNewTransaction(it) }
 //   }
 
 //   @PluginMethod
 //   fun platformInfo(call: PluginCall) {
-//     val value = call.getString("value")
+
+//     var clientVersion = SingletonClass.versionName
+
 //     val ret = JSObject()
-//     ret.put("value", value)
+//     ret.put("success", true)
+//     ret.put("platform", "ANDROID")
+//     ret.put("clientVersion", clientVersion)
 //     call.success(ret)
 //   }
 
 //   @PluginMethod
 //   fun viewAppPage(call: PluginCall) {
-//     val value = call.getString("value")
+//     var appId = call.getString("appId")
+//     var showReviewPage = call.getBoolean("showReviewPage", false)
+
+//     try {
+//       SingletonClass.openAppUrl(Uri.parse("market://details?id=$appId"))
+//     } catch (ex: ActivityNotFoundException) {
+//       SingletonClass.openAppUrl(Uri.parse("https://play.google.com/store/apps/details?id=$appId"))
+//     }
+
 //     val ret = JSObject()
-//     ret.put("value", value)
+//     ret.put("success", true)
 //     call.success(ret)
 //   }
 
 //   @PluginMethod
 //   fun playAudio(call: PluginCall) {
-//     val value = call.getString("value")
+//     var name = call.getString("name")
+
 //     val ret = JSObject()
-//     ret.put("value", value)
+//     ret.put("value", true)
 //     call.success(ret)
 //   }
-
-//   @PluginMethod
-//   fun openMailbox(call: PluginCall) {
-//     val value = call.getString("value")
-//     val ret = JSObject()
-//     ret.put("value", value)
-//     call.success(ret)
-//   }
-
-//   @PluginMethod
-//   fun vibrate(call: PluginCall) {
-//     val value = call.getString("value")
-//     val ret = JSObject()
-//     ret.put("value", value)
-//     call.success(ret)
-//   }
-
+// }
