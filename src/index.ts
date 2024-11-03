2edd37eccd3509e88afe7aed09842aa0ec449ae3
Ezeki Zibzibadze <ez@jok.io>
2024-11-03 20:34:47 +0400

    fix: bug

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import { registerPlugin } from '@capacitor/core'
 import type { JokHelperPlugin } from './definitions'
 import { JokHelperWeb } from './web'
 
-const JokHelper = registerPlugin<JokHelperPlugin>(
-  'JokHelper',
-  () => new JokHelperWeb(),
-)
+const JokHelper = registerPlugin<JokHelperPlugin>('JokHelper', {
+  web: () => new JokHelperWeb(),
+})
 
 export * from './definitions'
 export { JokHelper }
